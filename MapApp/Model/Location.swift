a4f58f7619b950461da86763967398e0528c7fcf
Maulik shah <maulik_shah@hotmail.co.in>
2025-02-07 10:57:24 +0530

    InitSetup done

---
+++ MapApp/Model/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Untitled.swift
+//  MapApp
+//
+//  Created by Maulik Shah on 2/7/25.
+//
+
+import Foundation
+import MapKit
+
+struct Location{
+    var name : String
+    var cityName : String
+    var coordinates : CLLocationCoordinate2D
+    var description : String
+    var imageNames : [String]
+    var link: String
+}
