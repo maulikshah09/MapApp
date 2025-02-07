4c27f2c487b749ca64308d55c65545dfaedb1511
Maulik shah <maulik_shah@hotmail.co.in>
2025-02-07 10:28:25 +0530

    first commit.

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Maulik Shah on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
